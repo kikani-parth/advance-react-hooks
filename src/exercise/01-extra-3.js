0fe50c0ad0311cfeb8e93025fe4c60c823c53113
Parth Kikani <parthkikani02@gmail.com>
2024-05-13 12:20:22 +0300

    complete 01.js extra credit 3, 4

---
+++ src/exercise/01-extra-3.js
@@ -0,0 +1,29 @@
+// useReducer: simple Counter
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from "react";
+
+function countReducer(prevCount, action) {
+  return {
+    ...prevCount,
+    ...(typeof action === "function" ? action(prevCount) : action),
+  };
+}
+
+function Counter({ initialCount = 0, step = 1 }) {
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  });
+
+  const { count } = state;
+  const increment = () =>
+    setState((currentState) => ({ count: currentState.count + step }));
+
+  return <button onClick={increment}>{count}</button>;
+}
+
+function App() {
+  return <Counter />;
+}
+
+export default App;
--- src/exercise/01-extra-4.js
+++ src/exercise/01-extra-4.js
@@ -1,9 +1,6 @@
 // useReducer: simple Counter
 // http://localhost:3000/isolated/exercise/01.js
 
-/* OVERWRITE THE RESPECTIVE EXERCISE SOLUTIONS IN THIS FILE TO VIEW THE PAGE ON THE ABOVE ADDRESS*/
-/* eg: COPY "01-exercise.js" FILE AND PASTE HERE TO LOAD ON THE ABOVE LOCALHOST ADDR */
-
 import * as React from "react";
 
 function countReducer(state, action) {
