675762783274dba827a82445b672581ff8fa3a55
Parth Kikani <parthkikani02@gmail.com>
2024-05-14 21:47:50 +0300

    complete 03.js exercise and extra credit 1

---
+++ src/exercise/03-exercise.js
@@ -0,0 +1,42 @@
+// useContext: simple Counter
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from "react";
+
+const CountContext = React.createContext();
+
+function CountProvider(props) {
+  const [count, setCount] = React.useState(0);
+
+  const value = [count, setCount];
+
+  return <CountContext.Provider value={value} {...props} />;
+}
+
+function CountDisplay() {
+  // get the count from useContext with the CountContext
+  const [count] = React.useContext(CountContext);
+  return <div>{`The current count is ${count}`}</div>;
+}
+
+function Counter() {
+  // get the setCount from useContext with the CountContext
+  const [, setCount] = React.useContext(CountContext);
+
+  const increment = () => setCount((c) => c + 1);
+
+  return <button onClick={increment}>Increment count</button>;
+}
+
+function App() {
+  return (
+    <div>
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
+    </div>
+  );
+}
+
+export default App;
